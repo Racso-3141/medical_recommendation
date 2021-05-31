80140cb6b1b39bec98861c788109a00ddf1d6f59
Racso-3141 <enxuhan@gmail.com>
2021-05-31 23:53:38 +0800

    remove redundant parts in recommendation model

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("#generate").onclick = function() {
+    document.querySelector("#generate_output").innerHTML = "Generate"
+};
+
+document.querySelector("#reset").onclick = function() {
+    document.querySelector("#reset_output").innerHTML = "Reset"
+}
